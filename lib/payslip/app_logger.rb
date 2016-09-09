86850367f111e47c8d040d5bb8f96f11f259a7a1
hww <836967373@qq.com>
2016-09-09 15:30:19 +0800

    add log lib

---
+++ lib/payslip/app_logger.rb
@@ -0,0 +1,11 @@
+module Payslip
+  class AppLogger < Payslip::Logger
+    def self.file_name
+      'application.log'
+    end
+
+    def format_message(severity, timestamp, progname, msg)
+      "#{timestamp.to_s(:long)}: #{msg}\n"
+    end
+  end
+end
