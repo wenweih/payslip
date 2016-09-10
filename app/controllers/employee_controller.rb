c684001c5ec2f82b56aaee4f1031608db9763c5f
hww <836967373@qq.com>
2016-09-11 02:32:26 +0800

    edit & destroy employee

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -5,14 +5,37 @@ class EmployeeController < ApplicationController
   end
 
   def edit
+    @employee = User.find(params[:id])
+  end
+
+  def update
+    params.permit!
+    user = User.find(params[:id])
+    begin
+      user.update_attributes!(params[:user])
+      redirect_to employee_index_path, notice: "update #{user.email} successfully"
+    rescue  StandardError => error
+      redirect_to :back, notice: "#{error}"
+    end
   end
 
   def destroy
+    user = User.find(params[:id])
+    begin
+      if user.namespace.present? && user.namespace.leader == user
+        redirect_to employee_index_path, notice: "#{user.email} is #{user.namespace.name} department, can't be deleted" and return
+      else
+        user.destroy!
+        redirect_to employee_index_path, notice: "deleted #{user.email}" and return
+      end
+    rescue  StandardError => error
+      redirect_to employee_index_path, notice: "#{error}"
+    end
   end
 
   def change_namespace
     user = User.find(params[:id])
-    # if user reassigned is a department leader ,change the department leader as admin
+    # if user reassigned is a department leader ,user admin user as the department leader
     if user.namespace.present? && user.namespace.leader == user
       user.namespace.leader = User.where(admin: 1).first
       user.namespace.save!
