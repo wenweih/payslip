03bfaba8e3ed07a851e048252abf207799f63e2c
hww <836967373@qq.com>
2016-09-10 17:09:32 +0800

    Sign up initiative or admin add employee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < Clearance::UsersController
+  skip_before_filter :redirect_signed_in_users, only: [:create], raise: false
+
   def new
     @user = user_from_params
   end
@@ -7,8 +9,12 @@ class UsersController < Clearance::UsersController
     @user = user_from_params
     begin
       @user.save!
-      sign_in @user
-      redirect_to dashboard_index_path, notice:"注册成功且自动登录" and return
+      if @user.first_name.present?
+        redirect_to :back, notice:"Successfully add #{@user.first_name}" and return
+      else
+        sign_in @user
+        redirect_to root_path, notice:"Successfully sign up and Sign in auto" and return
+      end
     rescue  ActiveRecord::RecordInvalid => exception
       flash[:notice] = "#{exception}"
       render "users/new"
@@ -17,16 +23,26 @@ class UsersController < Clearance::UsersController
 
   private
   def user_from_params
+    user_params = params[:user] || Hash.new
     email = user_params.delete(:email)
     password = user_params.delete(:password)
+    first_name = user_params.delete(:first_name)
+    last_name = user_params.delete(:last_name)
+    annual_salary = user_params.delete(:annual_salary)
+    super_rate = user_params.delete(:super_rate)
 
     Clearance.configuration.user_model.new(user_params).tap do |user|
       user.email = email
       user.password = password
+      user.first_name = first_name
+      user.last_name = last_name
+      user.annual_salary = annual_salary
+      user.super_rate = super_rate
     end
   end
 
-  def user_params
-    params[Clearance.configuration.user_parameter] || Hash.new
-  end
+  # def user_params
+  #   params.require(:user).permit(:email, :password, :first_name, :last_name, :annual_salary,  :super_rate)
+  #   # params[Clearance.configuration.user_parameter] || Hash.new
+  # end
 end
